7fcedfa72d49ce683df48c5eea4dda6cd55ee2ed
Wesley <wketchum@fnal.gov>
2020-01-31 09:59:28 -0600

    start adding in additional efield info into the tree

--- larsim/ElectronDrift/ShiftEdepSCE_module.cc
+++ larsim/ElectronDrift/ShiftEdepSCE_module.cc
@@ -51,10 +51,45 @@ private:
   // Declare member data here.
   art::InputTag fEDepTag;
   bool          fMakeAnaTree;
-  TNtuple*      fNtEdepAna;
 
-    //IS calculationg
-    larg4::ISCalcSeparate fISAlg;
+  //IS calculationg
+  larg4::ISCalcSeparate fISAlg;
+
+  TTree*      fEdepAnaTree;
+  typedef struct {
+
+    float energy;
+    
+    float orig_x;
+    float orig_y;
+    float orig_z;
+
+    float shift_x;
+    float shift_y;
+    float shift_z;
+    
+    float efield_bulk;
+    float efield_off_x;
+    float efield_off_y;
+    float efield_off_z;
+    float efield_sce;
+    
+    float scint_yield;
+
+    int orig_el;
+    int orig_ph_f;
+    int orig_ph_s;
+
+    int shift_el;
+    int shift_ph_f;
+    int shift_ph_s;
+
+    int trkid;
+    int pdg;
+
+  } EdepAna_t;
+  EdepAna_t fEdepAna;
+
 };
 
 
@@ -70,32 +105,20 @@ void spacecharge::ShiftEdepSCE::beginJob()
 {
   if(fMakeAnaTree){
     art::ServiceHandle<art::TFileService const> tfs;
-    fNtEdepAna = tfs->make<TNtuple>("nt_edep_ana","Edep PosDiff Ana Ntuple","energy:orig_x:orig_y:orig_z:orig_el:orig_ph:shift_x:shift_y:shift_z:shift_el:shift_ph");
+    fEdepAnaTree = tfs->make<TTree>("nt_edep_ana","Edep PosDiff Ana Ntuple");
+    fEdepAnaTree->Branch("edep",&fEdepAna,
+			 "energy:orig_x:orig_y:orig_z:shift_x:shift_y:shift_z:efield_bulk:efield_off_x:efield_off_y:efield_off_z:efield_sce:scint_yield:orig_el:orig_ph_f:orig_ph_s:shift_el:shift_ph_f:shift_ph_s:trkid:pdg");
   }
 
   art::ServiceHandle<sim::LArG4Parameters const> lg4paramHandle;
-//  fISAlg.Initialize(lar::providerFrom<detinfo::LArPropertiesService>(),
-//		    lar::providerFrom<detinfo::DetectorPropertiesService>(),
-//		    &(*lg4paramHandle),
-//		    lar::providerFrom<spacecharge::SpaceChargeService>());
   fISAlg.Initialize();
-
 }
 
 void spacecharge::ShiftEdepSCE::produce(art::Event & e)
 {
   art::ServiceHandle<sim::LArG4Parameters const> lg4paramHandle;
-//  fISAlg.Initialize(lar::providerFrom<detinfo::LArPropertiesService>(),
-//		    lar::providerFrom<detinfo::DetectorPropertiesService>(),
-//		    &(*lg4paramHandle),
-//		    lar::providerFrom<spacecharge::SpaceChargeService>());
-  fISAlg.Initialize();  /*
-  art::ServiceHandle<sim::LArG4Parameters const> lg4paramHandle;
-  fISAlg.Initialize(lar::providerFrom<detinfo::LArPropertiesService>(),
-		    lar::providerFrom<detinfo::DetectorPropertiesService>(),
-		    lg4paramHandle,
-		    lar::providerFrom<spacecharge::SpaceChargeService>());
-  */
+  fISAlg.Initialize();
+
   auto sce = lar::providerFrom<spacecharge::SpaceChargeService>();
 
   std::unique_ptr< std::vector<sim::SimEnergyDeposit> >
@@ -118,10 +141,8 @@ void spacecharge::ShiftEdepSCE::produce(art::Event & e)
     fISAlg.Reset();
     fISAlg.CalcIonAndScint(edep);
     outEdepVec.emplace_back(fISAlg.NumOfPhotons(),
-//                0,
-//                0,
 			    fISAlg.NumOfElectrons(),
-			    0.0,
+			    edep.ScintYieldRatio(),
 			    edep.Energy(),
 			    geo::Point_t{(float)(edep.StartX()-posOffsetsStart.X()), //x should be subtracted
 				(float)(edep.StartY()+posOffsetsStart.Y()),
@@ -133,12 +154,43 @@ void spacecharge::ShiftEdepSCE::produce(art::Event & e)
 			    edep.EndT(),
 			    edep.TrackID(),
 			    edep.PdgCode());
-    if(fMakeAnaTree)
+    if(fMakeAnaTree){
+
+      auto detp = lar::providerFrom<detinfo::DetectorPropertiesService>();
+
+      double efield = detp->Efield();
+      geo::Vector_t efieldOffsets{0.0,1.0,1.0};
+      if(sce->EnableSimEfieldSCE())
+	efieldOffsets = fSCE->GetEfieldOffsets(geo::Point_t{x,y,z});
+      double efield_sce = std::sqrt( (efield + efield*efieldOffsets.X())*(efield + efield*efieldOffsets.X()) +
+				     (efield*efieldOffsets.Y()*efield*efieldOffsets.Y()) +
+				     (efield*efieldOffsets.Z()*efield*efieldOffsets.Z()) );
+
+      fEdepAna.energy = edep.Energy();
+      fEdepAna.orig_x=edep.X();
+      fEdepAna.orig_y=edep.Y();
+      fEdepAna.orig_z=edep.Z();
+      fEdepAna.shift_x=outEdepVec.back().X();
+      fEdepAna.shift_y=outEdepVec.back().Y();
+      fEdepAna.shift_z=outEdepVec.back().Z();
+      fEdepAna.efield_bulk = detp->Efield();
+      fEdepAna.efield_off_x = efieldOffsets.X();
+      fEdepAna.efield_off_y = efieldOffsets.Y();
+      fEdepAna.efield_off_z = efieldOffsets.Z();
+      fEdepAna.efield_sce = efield_sce;
+      fEdepAna.scint_yield = edep.ScintYieldRatio();
+      fEdepAna.orig_el = edep.NumElectrons();
+      fEdepAna.orig_ph_f = edep.NumFPhotons();
+      fEdepAna.orig_ph_s = edep.NumSPhotons();
+      fEdepAna.shift_el = outEdepVec.back().NumElectrons();
+      fEdepAna.shift_ph_f = outEdepVec.back().NumFPhotons();
+      fEdepAna.shift_ph_s = outEdepVec.back().NumSPhotons();
+
       fNtEdepAna->Fill(edep.Energy(),
 		       edep.X(),edep.Y(),edep.Z(),edep.NumElectrons(),edep.NumPhotons(),
 		       outEdepVec.back().X(),outEdepVec.back().Y(),outEdepVec.back().Z(),
 		       outEdepVec.back().NumElectrons(),outEdepVec.back().NumPhotons());
-
+    }
 	//std::cout << "space charge position: (" << edep.X() << ", " << edep.Y() << ", " << edep.Z() << ") --> (" << outEdepVec.back().X() << ", " << outEdepVec.back().Y() << ", " << outEdepVec.back().Z() << ")" << std::endl;
   }
 
